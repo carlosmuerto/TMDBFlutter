200f298ecdfb5934cf13bd8fb2fd489a79692ada
Carlos Vivas <carlosmuerto@gmail.com>
2020-08-17 18:34:49 -0400

    intl/injectable initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:injectable/injectable.dart';
+
+import 'injection.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureInjection(Environment.prod);
   runApp(MyApp());
 }
 
